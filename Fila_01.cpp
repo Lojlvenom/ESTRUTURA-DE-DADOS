c580723fb9f3c9a84361d82a5852cc099e92fc14
Lojlvenom <49735852+Lojlvenom@users.noreply.github.com>
2019-10-30 15:38:09 -0400

    Add files via upload

---
+++ Fila_01.cpp
@@ -0,0 +1,63 @@
+//----------------------------------------------
+//          Estrutura de dados FILA 
+//----------------------------------------------
+
+/*
+ Definição:
+
+ Uma fila é uma estrutura de dados DINÂMICA que admite remoção e inserção de elementos (objetos).
+ Especificamente, uma  fila  (= queue)  é uma estrutura sujeita à seguinte regra de operação:
+ 
+ REGRA: O primeiro objeto inserido na fila é também o primeiro a ser removido. 
+        Essa política é conhecida pela sigla FIFO (= First-In-First-Out).
+
+ Operações da Fila:
+   1 - criação da fila;
+   2 - enfileirar;
+   3 - desenfileirar;
+   4 - mostrar a fila (todos os elementos);
+   5 - verificar se a fila está vazia (isEmpty);
+
+*/ 
+
+//----------------------------------------------
+//               Trabalho  
+//
+// Data da Entrega: 06/11/2019
+//----------------------------------------------
+
+/*
+
+ Faça a implementação de um programa que simule o atendimento de uma Fila de banco com
+ as seguintes especificação:
+ 
+ 1 - As operações:
+   1.1 - criação da fila;
+   1.2 - enfileirar;
+   1.3 - desenfileirar;
+   1.4 - mostrar a fila (todos os elementos);
+   1.5 - verificar se a fila está vazia (isEmpty);
+   
+ 2 - O programa terá duas Filas:  
+   2.1 - Fila de cliente comum
+   2.2 - Fila de cliente prioritário
+   2.3 - Deve-se atender primeiramente todos os clientes prioritários
+ 
+ 3 - Regras:
+   3.1 - O programa deve gerar automaticamente a sequencia dos números que representam 
+         as senhas de atendimento.
+   3.2 - O programa não pode ser finalizado sem atender todos os clientes      
+	 
+ 4 - Estrutura do Menu:
+   4.1 - Cliente Comum       : Inseri um cliente na fila comum
+   4.2 - Cliente Prioritário : Inseri um cliente na fila de prioridade 
+   4.3 - Atender cliente     : Remove um cliente da fila de prioridade ou comum conforme a demanda
+   4.4 - Listar cliente comum : A partir do primeiro cliente. 
+   4.5 - Listar cliente prioritário: A partir do primeiro cliente.
+   4.6 - Finalizar programa. 	 
+
+*/
+
+
+
+
